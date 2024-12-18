3d1749f805601ef1029e28fd5dcfb1a8bdc9d110
dvegaa20 <diego.vega.camacho@gmail.com>
2024-12-18 14:44:35 -0600

    feat(structure): added project structure

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignIn</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({});
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({});
